e060392876bd286478c65ac721e3048ccdd13ea7
Njugunasam <sammynjuguna5826@gmail.com>
2023-11-20 19:22:22 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/questions");
+        const data = await response.json();
+        setQuestions(data);
+      } catch (error) {
+        console.error("Error fetching questions:", error);
+      }
+    };
+
+    fetchQuestions();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`http://localhost:3000/questions/${id}`, {
+        method: "DELETE",
+      });
+
+      setQuestions((prevQuestions) =>
+        prevQuestions.filter((question) => question.id !== id)
+      );
+    } catch (error) {
+      console.error("Error deleting question:", error);
+    }
+  };
+
+  const handleCorrectIndexChange = async (id, newCorrectIndex) => {
+    try {
+      await fetch(`http://localhost:3000/questions/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ correctIndex: newCorrectIndex }),
+      });
+
+      setQuestions((prevQuestions) =>
+        prevQuestions.map((question) =>
+          question.id === id
+            ? { ...question, correctIndex: newCorrectIndex }
+            : question
+        )
+      );
+    } catch (error) {
+      console.error("Error updating correct index:", error);
+    }
+  };
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm setQuestions={setQuestions} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDelete={handleDelete}
+          onUpdateCorrectIndex={handleCorrectIndexChange}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,26 +1,43 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ setQuestions }) {
   const [formData, setFormData] = useState({
     prompt: "",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
+    answers: ["", "", "", ""],
     correctIndex: 0,
   });
 
-  function handleChange(event) {
+  const handleChange = (event) => {
     setFormData({
       ...formData,
       [event.target.name]: event.target.value,
     });
-  }
+  };
 
-  function handleSubmit(event) {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(formData);
-  }
+
+    try {
+      const response = await fetch("http://localhost:3000/questions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const newQuestion = await response.json();
+      setQuestions((prevQuestions) => [...prevQuestions, newQuestion]);
+
+      setFormData({
+        prompt: "",
+        answers: ["", "", "", ""],
+        correctIndex: 0,
+      });
+    } catch (error) {
+      console.error("Error creating new question:", error);
+    }
+  };
 
   return (
     <section>
@@ -34,55 +51,6 @@ function QuestionForm(props) {
             value={formData.prompt}
             onChange={handleChange}
           />
-        </label>
-        <label>
-          Answer 1:
-          <input
-            type="text"
-            name="answer1"
-            value={formData.answer1}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 2:
-          <input
-            type="text"
-            name="answer2"
-            value={formData.answer2}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 3:
-          <input
-            type="text"
-            name="answer3"
-            value={formData.answer3}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 4:
-          <input
-            type="text"
-            name="answer4"
-            value={formData.answer4}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Correct Answer:
-          <select
-            name="correctIndex"
-            value={formData.correctIndex}
-            onChange={handleChange}
-          >
-            <option value="0">{formData.answer1}</option>
-            <option value="1">{formData.answer2}</option>
-            <option value="2">{formData.answer3}</option>
-            <option value="3">{formData.answer4}</option>
-          </select>
         </label>
         <button type="submit">Add Question</button>
       </form>
